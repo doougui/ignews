4d73cd6df88ad478be4d45d967d8fa3e31eb96ad
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-09-15 16:54:51 -0300

    Getting product infodirectly from stripe using Next's SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import Head from 'next/head';
 import React from 'react';
 import Image from 'next/image';
+import { GetServerSideProps } from 'next';
 
 import styles from './home.module.scss';
 
 import girlCodingImg from '../../public/images/avatar.svg';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -20,13 +29,31 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
-        </section>
+          <SubscribeButton priceId={product.priceId} />
+        </section> 
 
         <Image src={girlCodingImg} alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Ja3jSGRueWLw5QSjkDwxRLL');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    }
+  }
+} 
