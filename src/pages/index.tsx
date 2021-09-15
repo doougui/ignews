b8bb741a2e07945ce3a4f43ed57bb940a18c05ab
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-09-15 16:20:30 -0300

    Creating SubscribeButton component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import styles from './home.module.scss';
 
 import girlCodingImg from '../../public/images/avatar.svg';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <Image src={girlCodingImg} alt="Girl coding" />
