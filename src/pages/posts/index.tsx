dfcf12c03e1c017271a4afca81620036db1b8897
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-10-12 19:17:45 -0300

    Getting prismic api data

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
 export default function Posts() {
@@ -10,17 +13,17 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
+          <a href="#">
             <time>12 de março de 2021</time>
             <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
             <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
           </a>
-          <a href="">
+          <a href="#">
             <time>12 de março de 2021</time>
             <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
             <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
           </a>
-          <a href="">
+          <a href="#">
             <time>12 de março de 2021</time>
             <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
             <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
@@ -29,4 +32,21 @@ export default function Posts() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.type', 'publication.content'],
+    pageSize: 100,
+  });
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {},
+  }
 }
