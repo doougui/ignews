ef2ed77bc889f5a16b2171b6074a45996989b4f5
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-10-13 22:13:58 -0300

    Typing GetStaticPaths

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Link from 'next/link';
 import Head from "next/head";
 import { RichText } from 'prismic-dom';
@@ -55,7 +55,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
     fallback: 'blocking',
@@ -82,5 +82,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   return {
     props: { post },
+    revalidate: 60 * 30, // 30 minutes
   }
 }
